c48b1f5ff4da9a44d2c81cee8272c1e21899b245
Kyrie <kyrieb@gmail.com>
2025-06-29 13:43:25 -0400

    Possible url layouts and manifest structures added for montecassino manuscripts, and added check box (no longer always true and invisible) for chunking out very large manuscripts

--- download copy/app.py
+++ download copy/app.py
@@ -58,3 +58,66 @@ def download():
 
 if __name__ == "__main__":
     app.run(debug=True)            # use a proper WSGI server in prod
+
+@app.route("/download")
+def download():
+    """
+    /download?ark=12148/btv1b55010551d&f=39&size=full
+    """
+    ark = request.args.get("ark")
+    fol = request.args.get("f")
+    size = request.args.get("size", "full")
+    
+    if not ark or not fol:
+        abort(400, "ark and f are required query parameters")
+    
+    # Determine if this is CNRS or Gallica based on ARK
+    if ark.startswith("63955/"):  # CNRS pattern
+        iiif_url = f"https://iiif.irht.cnrs.fr/iiif/ark:/{quote(ark)}/f{fol}/{size}/full/0/default.jpg"
+    else:  # Assume Gallica
+        iiif_url = f"https://gallica.bnf.fr/iiif/ark:/{quote(ark)}/f{fol}/{size}/full/0/default.jpg"
+    
+    data = fetch_from_server(iiif_url)  # You'll need to rename/update this function
+    filename = f"{ark.split('/')[-1]}_f{fol}.jpg"
+    
+    return Response(
+        data,
+        mimetype="image/jpeg",
+        headers={"Content-Disposition": f'attachment; filename="{filename}"'}
+    )
+@app.route("/download")
+def download():
+    ark = request.args.get("ark")
+    fol = request.args.get("f") 
+    size = request.args.get("size", "full")
+    
+    if not ark or not fol:
+        abort(400, "ark and f are required query parameters")
+    
+    # Try multiple URL patterns for CNRS
+    if ark.startswith("63955/"):  # CNRS pattern
+        # Try different CNRS URL patterns
+        url_patterns = [
+            f"https://iiif.irht.cnrs.fr/iiif/ark:/{quote(ark)}/f{fol}/{size}/full/0/default.jpg",
+            f"https://iiif.irht.cnrs.fr/iiif/ark:/{quote(ark)}/f{fol}/{size}/max/0/default.jpg", 
+            f"https://iiif.irht.cnrs.fr/iiif/ark:/{quote(ark)}/f{fol}/full/full/0/default.jpg",
+            f"https://iiif.irht.cnrs.fr/iiif/ark:/{quote(ark)}/f{fol}/full/max/0/default.jpg",
+            f"https://iiif.irht.cnrs.fr/iiif/ark:/{quote(ark)}/f{fol}"  # Base URL
+        ]
+        
+        for iiif_url in url_patterns:
+            try:
+                data = fetch_from_server(iiif_url)
+                if len(data) > 5000:  # Got actual image data
+                    break
+            except:
+                continue
+        else:
+            abort(404, "No working URL found for this CNRS image")
+    else:  # Assume Gallica
+        iiif_url = f"https://gallica.bnf.fr/iiif/ark:/{quote(ark)}/f{fol}/{size}/full/0/default.jpg"
+        data = fetch_from_server(iiif_url)
+    
+    filename = f"{ark.split('/')[-1]}_f{fol}.jpg"
+    return Response(data, mimetype="image/jpeg", 
+                   headers={"Content-Disposition": f'attachment; filename="{filename}"'})
